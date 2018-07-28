2b550e6ca731d24ec6a6e0115bfc7c537e762467
Jan Cassio <hey@jancassio.com>
2018-07-28 11:45:32 -0300

    fixed routing for production env

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import chalk from 'chalk';
 import express from 'express';
 import { AddressInfo } from 'net';
 import { error, info, ok } from '../scripts/lib/logger';
+import { react } from './server/middlewares/react';
 import { app, start } from './server/server';
 
 app.use(express.static(`dist/public`));
-app.use((req, res, next) => require('./server/router')(req, res, next));
+app.get('*', react());
 
 const run = async () => {
   try {
